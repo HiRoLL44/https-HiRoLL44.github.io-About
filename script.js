ab11d154f7395830db59e5a4df41e9496bc8de3a
HiRoLL44 <82860787+HiRoLL44@users.noreply.github.com>
2021-04-22 17:44:33 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('.hobby').ripples({
+    resolution: 600
+});
+$('.content').ripples({
+    resolution: 200
+});
+
+$('.a_content').ripples({
+    resolution: 200
+});
+
+
+
+
+});
